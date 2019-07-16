a66beaf947c877e552e82e848f0f22d6c19bc0f4
bendiksi <benibe88@gmail.com>
2019-07-16 16:49:08 +0200

    justerte parsing av datoer i sortering slik at den fungere med fix for safari

--- src/api/pindenaAPI.ts
+++ src/api/pindenaAPI.ts
@@ -2,10 +2,9 @@ import {Kurs} from "../models/Kurs";
 import {kursapiUrl} from "../utils/lenker";
 
 const sammenlignKursPaDato= (a:Kurs,b:Kurs) =>{
-    const aTime= a.RegistrationFromDateTime.replace("","T");
-    const bTime= b.RegistrationFromDateTime.replace("","T");
-
-    return new  Date(aTime).getTime() - new Date(bTime).getTime();
+    const aTime= a.RegistrationFromDateTime.replace(" ","T");
+    const bTime= b.RegistrationFromDateTime.replace(" ","T");
+    return (new Date(aTime).getTime() - new Date(bTime).getTime());
 };
 
 export async function hentKurs(): Promise<Kurs[]>{
@@ -13,6 +12,7 @@ export async function hentKurs(): Promise<Kurs[]>{
     if(response.ok){
         const kursJson = await response.json().then(data => data as Kurs[]);
         return kursJson.sort(sammenlignKursPaDato);
+
     }
     else return [];
 }
