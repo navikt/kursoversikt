4a95143a0d4ca9d4e1effa5251fadda26deda7a7
André Mossige <mossige.a@gmail.com>
2019-08-02 15:05:51 +0200

    Flytt om kurset til egen komponent

--- src/detaljside/DetaljSide.tsx
+++ src/detaljside/DetaljSide.tsx
@@ -2,26 +2,23 @@ import React, { FunctionComponent, useEffect, useState } from 'react';
 import { Kurs, tomtKurs } from '../models/Kurs';
 import { hentKurs } from '../api/pindenaAPI';
 import { RouteComponentProps } from 'react-router';
-import { Sidetittel, Systemtittel } from 'nav-frontend-typografi';
-import { Panel } from 'nav-frontend-paneler';
+import { Sidetittel } from 'nav-frontend-typografi';
 import 'nav-frontend-knapper-style';
-import { VenstreChevron } from 'nav-frontend-chevron';
-import Lenke from 'nav-frontend-lenker';
 import bemHelper from '../utils/bemHelper';
 import './DetaljSide.less';
 import Metainfo from './Metainfo/Metainfo';
+import OmKurset from './OmKurset/OmKurset';
 
 const cls = bemHelper('detaljside');
-const beskrivelseCls = bemHelper(cls.element('beskrivelsePanel'));
 
 const DetaljSide: FunctionComponent<RouteComponentProps> = props => {
-    const [detteKurset, setDetteKurset] = useState<Kurs>(tomtKurs);
+    const [kurs, setKurs] = useState<Kurs>(tomtKurs);
 
     useEffect(() => {
         const hentOgSettDetteKurset = async () => {
             const resultat = await hentKurs();
             let kursIdFraUrl = props.location.pathname.split('/')[1];
-            await setDetteKurset(
+            await setKurs(
                 resultat.filter(kurs => {
                     return kurs.id === parseInt(kursIdFraUrl);
                 })[0]
@@ -30,35 +27,14 @@ const DetaljSide: FunctionComponent<RouteComponentProps> = props => {
         hentOgSettDetteKurset();
     }, [props.location.pathname]);
 
-    const createMarkup = () => ({
-        __html: detteKurset.forsideBeskrivelse,
-    });
-
     return (
         <div className={cls.block}>
             <header className="overskrift">
-                <Sidetittel className="sentrertTekst">{detteKurset.tittel}</Sidetittel>
+                <Sidetittel className="sentrertTekst">{kurs.tittel}</Sidetittel>
             </header>
             <main className={cls.element('innhold')}>
-                <Metainfo kurs={detteKurset} />
-                <Panel className={beskrivelseCls.block}>
-                    <header className={'overskrift'}>
-                        <Systemtittel>Om kurset</Systemtittel>
-                    </header>
-                    <div dangerouslySetInnerHTML={createMarkup()} />
-                    <Lenke
-                        className="active knapp knapp--hoved"
-                        href={detteKurset.registreringsUrl}
-                    >
-                        Meld deg på
-                    </Lenke>
-                    <div className={beskrivelseCls.element('tilbakelenke')}>
-                        <Lenke href={'/kursoversikt'}>
-                            <VenstreChevron type={'venstre'} />
-                            Tilbake til kursoversikten
-                        </Lenke>
-                    </div>
-                </Panel>
+                <Metainfo kurs={kurs} />
+                <OmKurset kurs={kurs} />
             </main>
         </div>
     );
---
+++ src/detaljside/OmKurset/OmKurset.tsx
@@ -0,0 +1,40 @@
+import React, { FunctionComponent } from 'react';
+import { Panel } from 'nav-frontend-paneler';
+import { Systemtittel } from 'nav-frontend-typografi';
+import Lenke from 'nav-frontend-lenker';
+import { VenstreChevron } from 'nav-frontend-chevron';
+import { Kurs } from '../../models/Kurs';
+import './OmKurset.less';
+import bemHelper from '../../utils/bemHelper';
+
+const cls = bemHelper('omKurset');
+
+interface Props {
+    kurs: Kurs;
+}
+
+const OmKurset: FunctionComponent<Props> = ({ kurs }) => {
+    const beskrivelse = {
+        __html: kurs.forsideBeskrivelse,
+    };
+
+    return (
+        <Panel className={cls.block}>
+            <header className={cls.element('overskrift')}>
+                <Systemtittel>Om kurset</Systemtittel>
+            </header>
+            <div dangerouslySetInnerHTML={beskrivelse} />
+            <Lenke className="active knapp knapp--hoved" href={kurs.registreringsUrl}>
+                Meld deg på
+            </Lenke>
+            <div className={cls.element('tilbakelenke')}>
+                <Lenke href={'/kursoversikt'}>
+                    <VenstreChevron type={'venstre'} />
+                    Tilbake til kursoversikten
+                </Lenke>
+            </div>
+        </Panel>
+    );
+};
+
+export default OmKurset;
