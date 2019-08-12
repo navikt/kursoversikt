d537f4ae639df497c49aff970badc127ac85a352
bendiksi <benibe88@gmail.com>
2019-08-12 14:28:00 +0200

    fjernet et mellomrom

--- src/kursliste/Soketreff/Soketreff.tsx
+++ src/kursliste/Soketreff/Soketreff.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from 'react';
 import { Normaltekst } from 'nav-frontend-typografi';
 import bemHelper from '../../utils/bemHelper';
 import './Soketreff.less';
+
 interface Props {
     antallTreff: number;
     totaltAntallKurs: number;
@@ -12,7 +13,6 @@ const cls = bemHelper('soketreff');
 const Soketreff: FunctionComponent<Props> = ({ antallTreff, totaltAntallKurs }) => {
     return (
         <Normaltekst className={cls.block}>
-            {' '}
             <b>{antallTreff}</b> treff i <b>{totaltAntallKurs}</b> kurs
         </Normaltekst>
     );
