966f24ecb198ce31a0339d08d76529f2a946c6c7
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-08-28 11:02:10 +0200

    les secrets fra en fil

--- src/server/pindenaProxyConfig.js
+++ src/server/pindenaProxyConfig.js
@@ -1,10 +1,8 @@
 const proxy = require('http-proxy-middleware');
-const HttpsProxyAgent = require('https-proxy-agent');
 
 const envProperties = {
     API_USER: process.env.PINDENA_USER,
     API_PASS: process.env.PINDENA_PASS,
-    PROXY_SERVER: process.env.HTTPS_PROXY,
 };
 
 const pindenaProxyConfig = {
@@ -19,6 +17,7 @@ const pindenaProxyConfig = {
 };
 
 if (envProperties.API_USER) {
+    console.log("fant secrets i env ");
     pindenaProxyConfig.auth = envProperties.API_USER + ':' + envProperties.API_PASS;
 }
 
--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-if test -d /var/run/secrets/nais.io/vault;
+if test -d /var/run/secrets;
 then
-    for FILE in /var/run/secrets/nais.io/vault/*.env
+    for FILE in /var/run/secrets/
     do
         for line in $(cat $FILE); do
             echo "- exporting `echo $line | cut -d '=' -f 1`"
