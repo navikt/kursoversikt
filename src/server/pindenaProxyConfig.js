9e542c597670311eee6f07c85d1dab05be81a6c7
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-09-23 12:02:48 +0200

    endringer proxyconfig

--- src/server/pindenaProxyConfig.js
+++ src/server/pindenaProxyConfig.js
@@ -1,8 +1,10 @@
 const proxy = require('http-proxy-middleware');
+const HttpsProxyAgent = require('https-proxy-agent');
 
 const envProperties = {
     API_USER: process.env.PINDENA_USER,
     API_PASS: process.env.PINDENA_PASS,
+    PROXY_SERVER: process.env.HTTPS_PROXY,
 };
 
 const pindenaProxyConfig = {
@@ -17,7 +19,6 @@ const pindenaProxyConfig = {
 };
 
 if (envProperties.API_USER) {
-    console.log("fant secrets i env ");
     pindenaProxyConfig.auth = envProperties.API_USER + ':' + envProperties.API_PASS;
 }
 
