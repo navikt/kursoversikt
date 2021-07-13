db85168a3e4e4feb03d8b5615cb4a44fc5565064
Ibenholt <bendik.segrov.ibenholt@nav.no>
2021-07-13 13:51:16 +0200

    håndter alternativ tilrettelegging i tema filter spesielt og legg til filter på underkategori

--- src/kursliste/filtrertingsMotor.ts
+++ src/kursliste/filtrertingsMotor.ts
@@ -1,7 +1,7 @@
-import { Kurs } from '../models/Kurs';
+import {Kurs} from '../models/Kurs';
 import {FilterGruppe, FilterGruppeUtenFylke, FilterState} from './Kursliste';
 
-export const filterGruppeKeys: FilterGruppe[] = ['fylke', 'type', 'tema'];
+export const filterGruppeKeys: FilterGruppe[] = ['fylke', 'type', 'tema', "underkategori"];
 
 export const filtrerKurs = (
     filterState: FilterState,
@@ -10,8 +10,11 @@ export const filtrerKurs = (
 ): Kurs[] => {
     let filtrerteKurs = kursArray;
     filterGruppeKeys.map(filtergruppe => {
-        if(filtergruppe === 'fylke'){
-        return (filtrerteKurs = filtrerPaFylke(filtrerteKurs,filterState))
+        if (filtergruppe === 'fylke') {
+            return (filtrerteKurs = filtrerPaFylke(filtrerteKurs, filterState))
+        }
+        if (filtergruppe === 'underkategori') {
+            return (filtrerteKurs = filtrerPaUnderkategori(filtrerteKurs, filterState))
         }
         return (filtrerteKurs = utforFiltreringPaaFiltergruppe(
             filterState,
@@ -40,13 +43,22 @@ const utforFiltreringPaaFiltergruppe = (
     );
 };
 
-const filtrerPaFylke = (kursSomSkalFiltreres: Kurs[], filterState:FilterState ):Kurs[] => {
-    if(filterState.fylke.length === 0)
+const filtrerPaFylke = (kursSomSkalFiltreres: Kurs[], filterState: FilterState): Kurs[] => {
+    if (filterState.fylke.length === 0)
         return kursSomSkalFiltreres
     return kursSomSkalFiltreres.filter(
         kurs =>
             kurs.fylke &&
             (kurs.fylke.some(fylke => filterState['fylke'].includes(fylke)) || kurs.fylke!.includes('Landsdekkende'))
+    )
+}
+const filtrerPaUnderkategori = (kursSomSkalFiltreres: Kurs[], filterState: FilterState): Kurs[] => {
+    if (filterState.underkategori.length === 0)
+        return kursSomSkalFiltreres
+    return kursSomSkalFiltreres.filter(
+        kurs =>
+            kurs.underkategori &&
+            (kurs.underkategori.some(underkategori => filterState.underkategori.includes(underkategori)))
     )
 }
 
@@ -66,11 +78,18 @@ const utforSokIListe = (sokeordState: string, kursSomSkalFiltreres: Kurs[]): Kur
 export const lagFilterKriterier = (kursArray: Kurs[], filterGruppe: FilterGruppeUtenFylke): string[] => {
     const kursMedFilterGruppe = kursArray.filter(kurs => kurs[filterGruppe]);
     let unikeVerdierSet = new Set(kursMedFilterGruppe.map(kurs => kurs[filterGruppe]!));
-     return Array.from(unikeVerdierSet.values()).sort()
+    return Array.from(unikeVerdierSet.values()).sort()
 };
 
-export const lagFylkeFilterKriterier = (kursArray: Kurs[] ): string[] => {
+export const lagFylkeFilterKriterier = (kursArray: Kurs[]): string[] => {
     const fylkeflatmap = kursArray.flatMap(kurs => kurs.fylke!)
     let unikeVerdierSet = new Set(fylkeflatmap);
-    return  Array.from(unikeVerdierSet.values()).sort()
+    return Array.from(unikeVerdierSet.values()).sort()
+};
+
+export const lagUnderkategoriFilterKriterier = (kursArray: Kurs[]): string[] => {
+    const underkategoriflatmap = kursArray.flatMap(kurs => kurs.underkategori!)
+    let unikeVerdierSet = new Set(underkategoriflatmap);
+    console.log('lagUnderkategoriFilterKriterier unikeVerdierSet', unikeVerdierSet)
+    return Array.from(unikeVerdierSet.values()).sort()
 };
