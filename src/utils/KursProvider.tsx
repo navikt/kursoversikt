0931c8f4bc558a0921536de2df5d7dfa1e701146
bendiksi <bendik.segrov.ibenholt@nav.no>
2021-11-17 08:41:22 +0100

    dependency array i kursprovider

--- src/utils/KursProvider.tsx
+++ src/utils/KursProvider.tsx
@@ -1,28 +1,31 @@
-import React,{createContext, useEffect, useState} from "react";
+import React, {createContext, useEffect, useState} from "react";
 import {Kurs} from "../models/Kurs";
 import {hentAlleKurs} from "../api/kursAPI";
 
 export interface KursKontext {
     aktiveKurs: Kurs[],
     alleKurs: Kurs[],
-    kursLaster:boolean
+    kursLaster: boolean
 }
 
 
-export const KursListeContext = createContext<KursKontext>({aktiveKurs: [], alleKurs: [], kursLaster:true})
+export const KursListeContext = createContext<KursKontext>({aktiveKurs: [], alleKurs: [], kursLaster: true})
 
 export const KursProvider = (props: any) => {
-    const [kursKontext, setkursKontext] = useState<KursKontext>({aktiveKurs: [], alleKurs: [], kursLaster:true})
+    const [kursKontext, setkursKontext] = useState<KursKontext>({aktiveKurs: [], alleKurs: [], kursLaster: true})
     useEffect(() => {
             hentAlleKurs().then(alleKurs => {
-                setkursKontext({aktiveKurs: alleKurs, alleKurs: alleKurs.filter(kurs =>
-                        kurs.aktivt === 1
-
-                    ),kursLaster:false
-                })
+                    setkursKontext({
+                            aktiveKurs: alleKurs,
+                            alleKurs: alleKurs.filter(kurs =>
+                                kurs.aktivt === 1
+                            ),
+                            kursLaster: false
+                        }
+                    )
                 }
             )
-        }
+        },[]
     )
     return <KursListeContext.Provider value={kursKontext}>
         {props.children}
